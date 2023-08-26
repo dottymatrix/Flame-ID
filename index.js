689db850791e59208a507aaffb25504ae6e676dc
Abisoye Onanuga <102636953+dottymatrix@users.noreply.github.com>
2023-08-26 12:25:15 -0400

    Transaction setup

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
 require('dotenv').config();
 
-async function environmentSetup() {
+async function Environment() {
 
     //Grab your Hedera testnet account ID and private key from your .env file
-    const myAccountId = process.env.MY_ACCOUNT_ID;
-    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+    const myAccountId = process.env.HEDERA_ACCOUNT_ID;
+    const myPrivateKey = process.env.HEDERA_PRIVATE_KEY;
 
     // If we weren't able to grab it, we should throw a new error
     if (!myAccountId || !myPrivateKey) {
@@ -18,10 +18,36 @@ async function environmentSetup() {
     //Set your account as the client's operator
     client.setOperator(myAccountId, myPrivateKey);
 
-    //Set the default maximum transaction fee (in Hbar)
+    //Set the default maximum transaction fee & max query payment (in Hbar)
     client.setDefaultMaxTransactionFee(new Hbar(100));
-
-    //Set the maximum payment for queries (in Hbar)
     client.setMaxQueryPayment(new Hbar(50));
+
+    //Create new keys
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    //Create a new account with 1,000 tinybar starting balance
+    const newAccount = await new AccountCreateTransaction()
+        .setKey(newAccountPublicKey)
+        .setInitialBalance(Hbar.fromTinybars(1000))
+        .execute(client);
+
+    // Get the new account ID
+    const getReceipt = await newAccount.getReceipt(client);
+    const newAccountId = getReceipt.accountId;
+
+    //Log the account ID
+    console.log("The new account ID is: " +newAccountId);
+
+    //Verify the account balance
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId(newAccountId)
+        .execute(client);
+
+    //Log the account balance
+    console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
+
+    console.log('success');
+    client.close();
 }
-environmentSetup();
+Environment();
